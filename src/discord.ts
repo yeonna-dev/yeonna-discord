943b67af7ae0e2c119088c9eb322a1970951dc44
esfox <osairis100@gmail.com>
2021-11-14 10:50:37 +0800

    Update start script to Discord.js v13

--- src/discord.ts
+++ src/discord.ts
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-import { Client } from 'discord.js';
+import { Client, Intents } from 'discord.js';
 import { Comtroller } from 'comtroller';
 
 import { loadCommands } from './commands';
@@ -14,12 +14,30 @@ import { Log } from './utilities/logger';
     defaults: { prefix: ';' },
   });
 
-  const discordBot = new Client();
+  const discordBot = new Client({
+    intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_MEMBERS,
+      Intents.FLAGS.GUILD_BANS,
+      Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+      Intents.FLAGS.GUILD_INTEGRATIONS,
+      Intents.FLAGS.GUILD_WEBHOOKS,
+      Intents.FLAGS.GUILD_INVITES,
+      Intents.FLAGS.GUILD_VOICE_STATES,
+      Intents.FLAGS.GUILD_PRESENCES,
+      Intents.FLAGS.GUILD_MESSAGES,
+      Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+      Intents.FLAGS.GUILD_MESSAGE_TYPING,
+      Intents.FLAGS.DIRECT_MESSAGES,
+      Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+      Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+    ],
+  });
   discordBot.login(process.env.BOT_TOKEN);
 
   discordBot.on('ready', () => Log.info(`Discord bot connected as ${discordBot.user?.tag}`, true));
 
-  discordBot.on('message', message =>
+  discordBot.on('messageCreate', message =>
   {
     if(message.author.bot)
       return;
