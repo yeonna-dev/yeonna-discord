8ae9f89567e4216c974b483b2e25f7acca0c5b5f
esfox <osairis100@gmail.com>
2021-12-01 19:36:50 +0800

    Update `wheel` command

--- src/commands/wheel.ts
+++ src/commands/wheel.ts
@@ -1,32 +1,33 @@
-import { Message } from 'discord.js';
 import { Command, parseParamsToArray } from 'comtroller';
-
 import { updateUserPoints } from 'yeonna-core';
+
+import { DiscordMessage } from '../utilities/discord';
+import { Log } from '../utilities/logger';
 
 // TODO: Create per-server configurable data
 const reward = 350;
 const animals =
-[
-  { code: 'rb', name: 'Rabbit' },
-  { code: 'dr', name: 'Dragon' },
-  { code: 'pi', name: 'Pig' },
-  { code: 'sq', name: 'Squirrel' },
-  { code: 'uc', name: 'Unicorn' },
-  { code: 'pg', name: 'Penguin' },
-  { code: 'eg', name: 'Eagle' },
-  { code: 'tg', name: 'Tiger' },
-  { code: 'dg', name: 'Dog' },
-];
+  [
+    { code: 'rb', name: 'Rabbit' },
+    { code: 'dr', name: 'Dragon' },
+    { code: 'pi', name: 'Pig' },
+    { code: 'sq', name: 'Squirrel' },
+    { code: 'uc', name: 'Unicorn' },
+    { code: 'pg', name: 'Penguin' },
+    { code: 'eg', name: 'Eagle' },
+    { code: 'tg', name: 'Tiger' },
+    { code: 'dg', name: 'Dog' },
+  ];
 
 // TODO: Update messages
 export const wheel: Command =
 {
   name: 'wheel',
-  aliases: [ 'w' ],
-  run: async ({ message, params }: { message: Message, params: string }) =>
+  aliases: ['w'],
+  run: async ({ message, params }: { message: DiscordMessage, params: string; }) =>
   {
-    let [ pickedAnimal ] = parseParamsToArray(params);
-    if(! pickedAnimal)
+    let [pickedAnimal] = parseParamsToArray(params);
+    if(!pickedAnimal)
       return message.channel.send('Please choose an animal.');
 
     pickedAnimal = pickedAnimal.toLowerCase();
@@ -34,7 +35,7 @@ export const wheel: Command =
     const animal = animals.find(({ code, name }) =>
       code === pickedAnimal || name.toLowerCase() === pickedAnimal);
 
-    if(! animal)
+    if(!animal)
       return message.channel.send('You cannot choose that animal.');
 
     const sentMessage = await message.channel.send(`You chose **${animal.name}**.`
@@ -45,12 +46,21 @@ export const wheel: Command =
     const chosenAnimal = animals[Math.floor(Math.random() * animals.length)];
     const won = animal.code === chosenAnimal.code;
     if(won)
-      await updateUserPoints({
-        userIdentifier: message.author.id,
-        discordGuildID: message.guild?.id,
-        amount: reward,
-        add: true,
-      });
+    {
+      try
+      {
+        await updateUserPoints({
+          userIdentifier: message.author.id,
+          discordGuildID: message.guild?.id,
+          amount: reward,
+          add: true,
+        });
+      }
+      catch(error: any)
+      {
+        Log.error(error);
+      }
+    }
 
     await sentMessage.edit(`The wheel stopped at **${chosenAnimal.name}**.`
       + ` You ${won ? `win __**${reward}**__ points!` : 'lose.'}`);
