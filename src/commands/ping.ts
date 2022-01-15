a0cc85aa925906e616410a8db8d63d1a6bf54613
esfox <osairis100@gmail.com>
2022-01-15 11:35:04 +0800

    Update embed creation helper

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -2,14 +2,14 @@ import { Command } from 'comtroller';
 
 import { DiscordMessage } from '../utilities/discord';
 
-import { createEmbed } from '../helpers/createEmbed';
+import { createDiscordEmbed } from '../helpers/createEmbed';
 
 export const ping: Command =
 {
   name: 'ping',
   run: ({ message }: { message: DiscordMessage; }) =>
   {
-    const response = createEmbed(`${~~(message.client.ping)} ms`);
+    const response = createDiscordEmbed({ title: `${~~(message.client.ping)} ms` });
     message.channel.send({ embeds: [response] });
   },
 };
--- src/helpers/createEmbed.ts
+++ src/helpers/createEmbed.ts
@@ -1,12 +1,47 @@
-import { MessageEmbed } from 'discord.js';
+import { ColorResolvable, MessageEmbed } from 'discord.js';
 
-export function createEmbed(title: string, description?: string): MessageEmbed
+export function createDiscordEmbed({
+  title,
+  description,
+  color,
+  author,
+  timestamp,
+  footer,
+}: {
+  title?: string,
+  description?: string,
+  color?: string,
+  author?: {
+    name: string,
+    iconURL?: string,
+    url?: string,
+  };
+  timestamp?: number | Date | null,
+  footer?: {
+    text: string,
+    iconURL?: string;
+  };
+}): MessageEmbed
 {
-  const embed = new MessageEmbed()
-    .setTitle(title);
+  const embed = new MessageEmbed();
+
+  if(title)
+    embed.setTitle(title);
 
   if(description)
     embed.setDescription(description);
+
+  if(color)
+    embed.setColor(color as ColorResolvable);
+
+  if(author)
+    embed.setAuthor(author);
+
+  if(timestamp)
+    embed.setTimestamp(timestamp);
+
+  if(footer)
+    embed.setFooter(footer);
 
   return embed;
 }
