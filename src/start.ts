921cbfc931edb625698dfbab69a32a940cd44e12
esfox <osairis100@gmail.com>
2022-02-01 17:14:13 +0800

    Add global error handler for commands

--- src/start.ts
+++ src/start.ts
@@ -37,10 +37,18 @@ const guards = [isDisabled];
 
     comtroller.run(message.content, { message });
     Log.command(message);
+
+    /* Catch all errors and send a response */
+    process.on('uncaughtException', error =>
+    {
+      Log.error(error);
+      message.channel.send('Whoops. An unknown error occurred. Please contact esfox#2053.');
+    });
   });
 
   /* Listen to and handle message reactions */
   handleReactions(bot.client);
 
   startJobs(bot);
+
 })();
