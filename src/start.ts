56d44079c480990eac7516723e1ce0bb77fd8737
esfox <osairis100@gmail.com>
2022-02-22 15:55:00 +0800

    Implement per-guild command prefix

--- src/start.ts
+++ src/start.ts
@@ -18,23 +18,61 @@ import { Log } from './utilities/logger';
   const { config } = Config;
 
   const commands = await loadCommands();
+  const prefix = config.global.prefix || ';';
   const comtroller = new Comtroller({
     commands,
     defaults:
     {
-      prefix: config.global.prefix || ';',
+      prefix,
       guards: [isDisabled],
     },
   });
 
   const bot = new Discord();
-  bot.onMessage(message =>
+  bot.onMessage(async message =>
   {
     if(message.fromBot)
       return;
 
-    comtroller.run(message.content, { message });
-    Log.command(message);
+    let { content, guild } = message;
+
+    /*
+      If the command was done in a guild, get the guild's command prefix
+      and check if the message content starts with the guild's prefix.
+      If it does, replace the start of the message content (the prefix part)
+      with the global prefix to trigger the command.
+      If if starts with the global prefix, do not run the command.
+    */
+    if(guild.id)
+    {
+      let guildPrefix;
+      try
+      {
+        const guildConfig = await Config.ofGuild(guild.id);
+        guildPrefix = guildConfig.prefix;
+      }
+      catch(error)
+      {
+        Log.error(error);
+      }
+
+      if(!guildPrefix || content.startsWith(prefix))
+        return;
+
+      if(content.startsWith(guildPrefix))
+        content = content.replace(guildPrefix, prefix);
+    }
+
+    try
+    {
+      const command = await comtroller.run(content, { message });
+      if(command)
+        Log.command(message);
+    }
+    catch(error)
+    {
+      Log.error(error);
+    }
 
     /* Catch all errors */
     process.on('uncaughtException', error => Log.error(error));
