7387af4bd37ccf125623822e0edacdcbda3d0953
esfox <osairis100@gmail.com>
2022-02-25 07:24:18 +0800

    Add cooldown for wheel command

--- src/commands/wheel.ts
+++ src/commands/wheel.ts
@@ -1,31 +1,47 @@
 import { Command, parseParamsToArray } from 'comtroller';
 import { Core } from 'yeonna-core';
 
+import { cooldowns, checkCooldownInGuild } from '../cooldowns';
+import { getTimeLeft } from '../helpers/getTimeLeft';
+
 import { Discord } from '../utilities/discord';
 import { Log } from '../utilities/logger';
 
 // TODO: Create per-server configurable data
 const reward = 350;
-const animals =
-  [
-    { code: 'rb', name: 'Rabbit' },
-    { code: 'dr', name: 'Dragon' },
-    { code: 'pi', name: 'Pig' },
-    { code: 'sq', name: 'Squirrel' },
-    { code: 'uc', name: 'Unicorn' },
-    { code: 'pg', name: 'Penguin' },
-    { code: 'eg', name: 'Eagle' },
-    { code: 'tg', name: 'Tiger' },
-    { code: 'dg', name: 'Dog' },
-  ];
+const animals = [
+  { code: 'rb', name: 'Rabbit' },
+  { code: 'dr', name: 'Dragon' },
+  { code: 'pi', name: 'Pig' },
+  { code: 'sq', name: 'Squirrel' },
+  { code: 'uc', name: 'Unicorn' },
+  { code: 'pg', name: 'Penguin' },
+  { code: 'eg', name: 'Eagle' },
+  { code: 'tg', name: 'Tiger' },
+  { code: 'dg', name: 'Dog' },
+];
+
+const name = 'wheel';
+
+/* Add 60 second cooldown. */
+cooldowns.add(name, 60000);
 
 // TODO: Update messages
 export const wheel: Command =
 {
-  name: 'wheel',
+  name,
   aliases: ['w'],
   run: async ({ discord, params }: { discord: Discord, params: string, }) =>
   {
+    const userIdentifier = discord.getAuthorId();
+    const discordGuildId = discord.getGuildId();
+    if(!discordGuildId)
+      return;
+
+    const cooldown = await checkCooldownInGuild(name, discordGuildId, userIdentifier);
+    if(cooldown)
+      return discord.send(`Please wait ${getTimeLeft(cooldown)}.`);
+
     let [pickedAnimal] = parseParamsToArray(params);
     if(!pickedAnimal)
       return discord.send('Please choose an animal.');
@@ -50,8 +66,8 @@ export const wheel: Command =
       try
       {
         await Core.Users.updatePoints({
-          userIdentifier: discord.getAuthorId(),
-          discordGuildId: discord.getGuildId(),
+          userIdentifier,
+          discordGuildId,
           amount: reward,
           add: true,
         });
