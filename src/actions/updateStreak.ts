329fdeb4c98a3500193a2e4f29d6bbafce5d9062
esfox <osairis100@gmail.com>
2022-04-14 10:07:27 +0800

    Add streak short notice

--- src/actions/updateStreak.ts
+++ src/actions/updateStreak.ts
@@ -77,13 +77,21 @@ export async function updateStreak({
 
   const previousCount = previous?.count || 0;
   const previousCountString = `${previousCount} ${streaksName}`;
-
   const currentCountString = `${current.count} ${streaksName}`;
 
-  discord.reply(
-    `Updated your streak from`
-    + ` ${previousCountString} ➡️ **${currentCountString}**`
-  );
+  const replyEmbed = discord.createDiscordEmbed({
+    title: `Updated your streak from ${previousCountString} ➡️ __${currentCountString}__`,
+  });
+
+  /* Temporary short notice for a server. */
+  if(discordGuildId === '696448387964469339')
+    replyEmbed.setDescription(
+      '\n\n\nIf your current streak is inaccurate, please adjust it accordingly using `!set (number)`.'
+      + '\n[Please read this post in #announcements]'
+      + '(https://discord.com/channels/696448387964469339/761613806933245982/963645083717091338)'
+    );
+
+  discord.replyEmbed(replyEmbed);
 
   const streakCount = current.count;
   if(!streaksRoles)
