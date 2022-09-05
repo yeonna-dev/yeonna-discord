8488e957b5e14d02ef03688e463788dc41a860b2
esfox <osairis100@gmail.com>
2022-09-05 12:16:17 +0800

    Update bag command response

--- src/commands/items/bag.command.ts
+++ src/commands/items/bag.command.ts
@@ -2,7 +2,6 @@ import { Command } from 'comtroller';
 import { Discord } from 'src/libs/discord';
 import { Log } from 'src/libs/logger';
 import { ItemsCommandResponse } from 'src/responses/items';
-import { table } from 'table';
 import { Core } from 'yeonna-core';
 
 export const bag: Command =
@@ -32,63 +31,6 @@ export const bag: Command =
     if(!items || !items.length)
       return response.noItems();
 
-    let totalAmount = 0;
-    let totalCost = 0;
-    const inventoryTableData = [];
-    for(const { name, amount, price, category } of items)
-    {
-      if(!price || !name)
-        continue;
-
-      totalAmount += amount;
-      totalCost += amount * price;
-      inventoryTableData.push([
-        name.length >= 20 ? `${name.substring(0, 19)}…` : name,
-        category,
-        amount,
-        amount * price,
-      ]);
-    }
-
-    // TODO: Add in responses?
-    const tableData = [
-      ['Item', 'Category', 'Amount', 'Cost'],
-      ...inventoryTableData,
-      ['', 'TOTAL', totalAmount, totalCost],
-    ];
-
-    const bag = '```ml\n'
-      + table(tableData, {
-        border:
-        {
-          topBody: `─`,
-          topJoin: `┬`,
-          topLeft: `┌`,
-          topRight: `┐`,
-
-          bottomBody: `─`,
-          bottomJoin: `┴`,
-          bottomLeft: `└`,
-          bottomRight: `┘`,
-
-          bodyLeft: `│`,
-          bodyRight: `│`,
-          bodyJoin: `│`,
-
-          joinBody: `─`,
-          joinLeft: `├`,
-          joinRight: `┤`,
-          joinJoin: `┼`
-        },
-        drawHorizontalLine: (index, size) => (
-          index === 0
-          || index === 1
-          || index === size - 1
-          || index === size
-        ),
-      })
-      + '\n```';
-
-    discord.reply(bag);
+    response.items(items);
   },
 };
--- src/responses/items.ts
+++ src/responses/items.ts
@@ -1,6 +1,8 @@
 import { Discord } from 'src/libs/discord';
 import { CommandResponse } from 'src/responses/common';
+import { table } from 'table';
 import { Collection, UserCollection } from 'yeonna-core/dist/modules/items/services/CollectionsService';
+import { InventoryItem } from 'yeonna-core/dist/modules/items/services/InventoriesService';
 import { Item } from 'yeonna-core/dist/modules/items/services/ItemsService';
 
 export class ItemsCommandResponse extends CommandResponse
@@ -31,6 +33,72 @@ export class ItemsCommandResponse extends CommandResponse
       title: 'Congratulations!',
       description: `${response}\nYou earn a bonus of __**${totalBonus} points**__!`,
     });
+  };
+
+  items = (items: InventoryItem[]) =>
+  {
+    const tableHeader = ['Item', 'Category', 'Amount', 'Cost'];
+    const tableBody = [];
+    let totalAmount = 0;
+    let totalCost = 0;
+    for(const { name, category, amount, price } of items)
+    {
+      if(!name || !price)
+        return;
+
+      totalAmount += amount;
+      totalCost += amount * price;
+      tableBody.push([
+        name.length >= 20 ? `${name.substring(0, 19)}…` : name,
+        category,
+        amount,
+        amount * price,
+      ]);
+    }
+
+    const tableFooter = ['', 'TOTAL', totalAmount, totalCost];
+    const tableData = [
+      tableHeader,
+      ...tableBody,
+      tableFooter,
+    ];
+
+    const itemsTable = table(tableData, {
+      border:
+      {
+        topBody: `─`,
+        topJoin: `┬`,
+        topLeft: `┌`,
+        topRight: `┐`,
+
+        bottomBody: `─`,
+        bottomJoin: `┴`,
+        bottomLeft: `└`,
+        bottomRight: `┘`,
+
+        bodyLeft: `│`,
+        bodyRight: `│`,
+        bodyJoin: `│`,
+
+        joinBody: `─`,
+        joinLeft: `├`,
+        joinRight: `┤`,
+        joinJoin: `┼`
+      },
+      drawHorizontalLine: (index, size) => (
+        index === 0
+        || index === 1
+        || index === size - 1
+        || index === size
+      ),
+    });
+
+    return this.discord.reply(
+      '**Current Items**'
+      + '\n```ml\n'
+      + itemsTable
+      + '\n```'
+    );
   };
 
   collections = (collections: UserCollection[]) =>
