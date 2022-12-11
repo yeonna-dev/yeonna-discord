190ef7d4f484076dbe99f6df4accb2b1f1fc7402
esfox <osairis100@gmail.com>
2022-12-11 16:29:57 +0800

    Add time logs list command

--- src/commands/timelogs/timelogs.command.ts
+++ src/commands/timelogs/timelogs.command.ts
@@ -1,5 +1,9 @@
 import { Command } from 'comtroller';
 import { Discord } from 'src/libs/discord';
+import { Log } from 'src/libs/logger';
+import { TimeLogsCommandResponse } from 'src/responses/timeLogs';
+import { Core } from 'yeonna-core';
+import { TimeLog } from 'yeonna-core/dist/modules/timeLogs/services/TimeLogService';
 
 export const timelogs: Command =
 {
@@ -7,14 +11,32 @@ export const timelogs: Command =
   aliases: ['tl'],
   run: async ({ discord }: { discord: Discord; }) =>
   {
+    const response = new TimeLogsCommandResponse(discord);
+
     const userIdentifier = discord.getAuthorId();
     const discordGuildId = discord.getGuildId();
+    let timeLogs: TimeLog[];
     try
     {
+      const timeLogsData = await Core.TimeLogs.get({
+        userIdentifier,
+        discordGuildId,
+      });
 
-    } catch(error)
-    {
+      if(!timeLogsData)
+        return;
 
+      timeLogs = timeLogsData;
     }
+    catch(error)
+    {
+      Log.error(error);
+      return response.cannotGetTimelogs();
+    }
+
+    if(!timeLogs)
+      return;
+
+    response.showTimeLogs(timeLogs);
   },
 };
--- src/responses/timeLogs.ts
+++ src/responses/timeLogs.ts
@@ -1,5 +1,6 @@
 import { Discord } from 'src/libs/discord';
 import { CommandResponse } from 'src/responses/common';
+import { TimeLog } from 'yeonna-core/dist/modules/timeLogs/services/TimeLogService';
 
 export class TimeLogsCommandResponse extends CommandResponse
 {
@@ -30,7 +31,36 @@ export class TimeLogsCommandResponse extends CommandResponse
     ],
   });
 
+  showTimeLogs = (timeLogs: TimeLog[]) =>
+  {
+    const itemsPerPage = 5;
+    const createPage = ({ pageItems }: { pageItems: TimeLog[]; }) =>
+    {
+      const pageContent = pageItems
+        .map(({ datetime, activity }) =>
+        {
+          const time = new Date(datetime).getTime();
+          return `<t:${Math.floor(time / 1000)}>\n${activity}`;
+        })
+        .join('\n\n');
+
+      return `Time logs\n\n${pageContent}`;
+    };
+
+    const userIdentifier = this.discord.getAuthorId();
+    return this.discord.sendPaginated({
+      createPage,
+      data: timeLogs,
+      itemsPerPage,
+      pageControlUserIds: [userIdentifier],
+    });
+  };
+
   cannotAddTimeLog = () => this.discord.replyEmbed({
     title: 'An error occurred in adding the time log. Please try again.'
+  });
+
+  cannotGetTimelogs = () => this.discord.replyEmbed({
+    title: 'An error occurred in getting time logs. Please try again.'
   });
 }
