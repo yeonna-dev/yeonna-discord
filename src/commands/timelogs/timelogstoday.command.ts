0c5bf76c82356d7cb1900584bc10f45790e35b7a
esfox <osairis100@gmail.com>
2023-01-22 09:05:08 +0800

    temp (failing yeonna config install)

---
+++ src/commands/timelogs/timelogstoday.command.ts
@@ -0,0 +1,42 @@
+import { Command } from 'comtroller';
+import { Discord } from 'src/libs/discord';
+import { Log } from 'src/libs/logger';
+import { TimeLogsCommandResponse } from 'src/responses/timeLogs';
+import { Core } from 'yeonna-core';
+import { TimeLog } from 'yeonna-core/dist/modules/timeLogs/services/TimeLogService';
+
+export const timelogstoday: Command =
+{
+  name: 'timelogstoday',
+  aliases: ['tlt'],
+  run: async ({ discord }: { discord: Discord; }) =>
+  {
+    const response = new TimeLogsCommandResponse(discord);
+
+    const userIdentifier = discord.getAuthorId();
+    const discordGuildId = discord.getGuildId();
+    let timeLogs: TimeLog[];
+    try
+    {
+      const timeLogsData = await Core.TimeLogs.get({
+        userIdentifier,
+        discordGuildId,
+      });
+
+      if(!timeLogsData)
+        return;
+
+      timeLogs = timeLogsData;
+    }
+    catch(error)
+    {
+      Log.error(error);
+      return response.cannotGetTimelogs();
+    }
+
+    if(!timeLogs)
+      return;
+
+    response.showTimeLogs(timeLogs);
+  },
+};
